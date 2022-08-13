e14fd316a0cb64d9b42e9b0685fb8b1e15e6644d
Divijasaini18 <65226784+Divijasaini18@users.noreply.github.com>
2022-08-13 14:30:49 +0530

    Add files via upload

---
+++ favorite/src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+
+
+@EnableWebMvc
+@OpenAPIDefinition
+@SpringBootApplication
+// @EnableEurekaClient
+public class App {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
